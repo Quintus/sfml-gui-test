1a4008780ec5abe4ac1a4c76ca0d038b1e83ef82
Quintus <quintus@quintilianus.eu>
2016-01-10 17:25:16 +0100

    Add example using XML layout instead of code.

---
+++ src/xml_window_creation.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <CEGUI/CEGUI.h>
+#include <CEGUI/RendererModules/OpenGL/GLRenderer.h>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+  // Create SFML window
+  sf::RenderWindow window(sf::VideoMode(1024, 780), "CEGUI Test");
+  window.setMouseCursorVisible(false); // Drawn with CEGUI
+
+  // Some example scene drawn with SFML
+  sf::CircleShape circle(50);
+  circle.setFillColor(sf::Color::Green);
+
+  // Instruct CEGUI to use the OpenGL renderer and the default (filesystem) resource provider
+  CEGUI::OpenGLRenderer& cegui_renderer = CEGUI::OpenGLRenderer::bootstrapSystem();
+  CEGUI::DefaultResourceProvider* p_rp = static_cast<CEGUI::DefaultResourceProvider*>(CEGUI::System::getSingleton().getResourceProvider());
+
+  // Setup base pathes
+  p_rp->setResourceGroupDirectory("schemes", "../data/schemes");
+  p_rp->setResourceGroupDirectory("imagesets", "../data/imagesets");
+  p_rp->setResourceGroupDirectory("fonts", "../data/fonts");
+  p_rp->setResourceGroupDirectory("layouts", "../data/layouts");
+  p_rp->setResourceGroupDirectory("looknfeels", "../data/looknfeels");
+
+  // map path groups to the CEGUI system
+  CEGUI::Scheme::setDefaultResourceGroup("schemes");
+  CEGUI::ImageManager::setImagesetDefaultResourceGroup("imagesets");
+  CEGUI::Font::setDefaultResourceGroup("fonts");
+  CEGUI::WindowManager::setDefaultResourceGroup("layouts");
+  CEGUI::WidgetLookManager::setDefaultResourceGroup("looknfeels");
+
+  // Select TaharezLook as our GUI theme
+  CEGUI::SchemeManager::getSingleton().createFromFile("TaharezLook.scheme");
+
+  // Have CEGUI draw a mouse cursor
+  CEGUI::GUIContext& gui_context = CEGUI::System::getSingleton().getDefaultGUIContext();
+  gui_context.getMouseCursor().setDefaultImage("TaharezLook/MouseArrow");
+
+  // Load the window layout from XML
+  CEGUI::Window* p_rootwindow = CEGUI::WindowManager::getSingleton().loadLayoutFromFile("test.layout");
+  gui_context.setRootWindow(p_rootwindow);
+
+  // Main loop
+  bool terminate = false;
+  while (!terminate) {
+    sf::Event evt;
+
+    // Dispatch events
+    while (window.pollEvent(evt)) {
+      switch(evt.type) {
+      case sf::Event::Closed:
+	terminate = true;
+	break;
+      case sf::Event::MouseMoved:
+	// Tell CEGUI about mouse position changes
+	gui_context.injectMousePosition(evt.mouseMove.x, evt.mouseMove.y);
+	break;
+      case sf::Event::MouseButtonPressed:
+	// Tell CEGUI about left mouse button pressed
+	if (evt.mouseButton.button == sf::Mouse::Left) {
+	  gui_context.injectMouseButtonDown(CEGUI::LeftButton);
+	}
+	break;
+      case sf::Event::MouseButtonReleased:
+	// Tell CEGUI about left mouse button released
+	if (evt.mouseButton.button == sf::Mouse::Left) {
+	  gui_context.injectMouseButtonUp(CEGUI::LeftButton);
+	}
+	break;
+      default:
+	// ignore
+	break;
+      }
+    }
+
+    // Draw the SFML scecne
+    window.clear(sf::Color::Black);
+    window.draw(circle);
+
+    // Draw CEGUI after the rest (with SFML OpenGL reset applied)
+    window.pushGLStates();
+    CEGUI::System::getSingleton().renderAllGUIContexts();
+    window.popGLStates();
+
+    window.display();
+  }
+
+  // Finish
+  window.close();
+  return 0;
+}
